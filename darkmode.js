3c00f9ddc3432ff1793b05cf63ba0a5a6d4dda01
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2021-12-30 14:46:49 +0200

    Updatea
    Nimet, linkit jne

---
+++ darkmode.js
@@ -0,0 +1,7 @@
+/* <!-- Required DarkMode JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
+<script>
+    document.querySelector("darkmode").onclick = function (e) {
+        darkmode.toggleDarkMode();
+    };
+</script> */
